0880f06855f01fd6d80c43a0a4b0a77ca57567aa
Matteo Scarpone <m.scarpone@campus.unimib.it>
2016-02-27 21:37:33 +0100

    Set holidays utility

--- utils/populate.py
+++ utils/populate.py
@@ -1,7 +1,7 @@
 import sys
 import hashlib
 import json
-
+import pprint
 activity_file = sys.path[1] + "/database/.json"
 group_file = sys.path[1] + "/database/group.json"
 location_file = sys.path[1] + "/database/location.json"
@@ -20,6 +20,38 @@ def populate_user(username, password, email, name, surname):
     user["holiday"] = []
     return user
 
+def setholidays(user, holidays):
+    """
+    :type user: int
+    :type holidays = dictionary
+    example: setholidays(1, {"begin":timestamp,"end":timestamp})
+    """
+    # Open user file and load data
+    datafile = open(user_file)
+    data = json.load(datafile)
+    datafile.close()
+    # Find user ID
+    x = i = 0
+    found = False
+    holidays_list = []
+    while (found == False) and (x < len(data)):
+        if user == data[x]["ID"]:
+            holidays_list = data[x]["holiday"]
+            found = True
+            # Load last holiday ID
+            while i < len(holidays_list) and i == holidays_list[i]["ID"]:
+                i += 1
+            # Set holiday id
+            holidays["ID"] = i
+            # Add new holiday
+            holidays_list.append(holidays)
+            print holidays_list
+            #print(data)
+        x += 1
+        data[x]["holiday"] = holidays_list
+        datafile = open(user_file, "w")
+        json.dump(data,datafile)
+        datafile.close()
 
 def add_dict(dictionary, path):
     id = []
