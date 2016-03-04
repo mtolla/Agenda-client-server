4196889c5308eda004ea2f33e7d7f8c932bb0606
Matteo Scarpone <m.scarpone@campus.unimib.it>
2016-03-04 15:55:44 +0100

    Startserver end project.py

--- startServer.py
+++ startServer.py
@@ -3,7 +3,6 @@ import os
 
 project_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(project_path)
+from server.index import *
 
-from client.login_manager import __init__
-
-__init__()
+index.run()
