cef601e0493eec8ca9134b97d56ba929796513b1
Stefano Ravetta <s.ravetta94@gmail.com>
2016-03-05 12:49:53 +0100

    risolto problema path

--- client/local/file_location.py
+++ client/local/file_location.py
@@ -3,16 +3,16 @@ import os
 project_path = os.path.dirname(os.path.abspath(__file__))
 print "Il tuo file si trova qui: " + project_path
 path = "/".join(project_path.split("\\")[:-1])
-print "Il nuovo path e' in: " + path
-print "Local save si trova qui: " + path + "/local/local_save.json"
+print "l'ALERT si trova in: " + project_path + "/../gui/icon/alert.png"
+print "Local save si trova qui: " + project_path + "/local_save.json"
 
 # Icone
-ALERT = path + "/gui/icon/alert.png"
-INFO = path + "/gui/icon/info.png"
-MANAGE_IT = path + "/gui/icon/ManageIT.png"
-QUIT = path + "/gui/icon/quit.png"
-LOGOUT = path + "/gui/icon/logout.png"
-NOTIFICATION = path + "/gui/icon/notifications.png"
+ALERT = project_path + "/../gui/icon/alert.png"
+INFO = project_path + "/../gui/icon/info.png"
+MANAGE_IT = project_path + "/../gui/icon/ManageIT.png"
+QUIT = project_path + "/../gui/icon/quit.png"
+LOGOUT = project_path + "/../gui/icon/logout.png"
+NOTIFICATION = project_path + "/../gui/icon/notifications.png"
 
 # Altro
 LOCAL_SAVE = project_path + "/local_save.json"
