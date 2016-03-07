e2306273aa76d34f096f0f993adc0ac1339ad417
tolla94 <mtt.tollari@gmail.com>
2016-03-07 17:23:04 +0100

    Inizio implementazione server per il client

--- client/server_manager/server_notification.py
+++ client/server_manager/server_notification.py
@@ -2,14 +2,12 @@
 from flask import Flask
 from flask import request
 import json
-from data import data
 
-requests = Flask(__name__)
-data = data()
-requests.run(host=data.ip, port=data.port, debug=True)
+notification = Flask(__name__)
+notification.run(host=data.ip, port=data.port, debug=True)
 
 
-@requests.route("/")
+@notification.route("/")
 def notification():
     act = json.loads(request.form['user'])
     print act
